b28f6abae25e1828c4a8deac0755277d29764b99
wu <rty21785@gmail.com>
2024-03-11 02:26:02 +0800

    feat(auth): auth store調整

--- src/stores/userProfile.ts
+++ src/stores/userProfile.ts
@@ -1,10 +1,10 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const userProfileStores = defineStore('counter', () => {
-  const user = ref(null)
-  const authOn = ref(false)
-  const messageOnValue = ref(false)
+export const UserProfileStores = defineStore('counter', () => {
+  const user = ref<any>(null)
+  const authOn = ref<boolean>(false)
+  const messageOnValue = ref<boolean>(false)
 
   return { user, authOn, messageOnValue }
 })
