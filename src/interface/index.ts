8cde60598b47bb285b3f97f79e1f9087d9ef693b
wu <rty21785@gmail.com>
2024-03-19 20:21:10 +0800

    feat(table): export type型別

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,5 +1,5 @@
 export type { User } from './User'
 export type { StringObject } from './Universal'
-export type { Columns } from './Table'
+export type { Columns, TypeItem } from './Table'
 export type { LayoutConfig, LayoutState } from './Layout'
 export type { NewRouteRecordRaw } from './Router'
