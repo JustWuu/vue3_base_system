d65377e60f057c2ea31ff053bf20b06bc7b434c0
wu <rty21785@gmail.com>
2024-03-23 02:11:25 +0800

    refactor(router): 重構路由守衛

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import { UserProfileStores } from '@/stores/userProfile'
+import { UserStore } from '@/stores'
 import AppLayout from '@/layout/AppLayout.vue'
+import type { User } from '@/interface'
+import { UserObject } from '@/interface'
 
 // router
 import systemRouter from './modules/system'
@@ -113,86 +115,66 @@ const router = createRouter({
 // https://book.vue.tw/CH4/4-4-navigation-guards.html
 // 路由守衛
 router.beforeEach((to) => {
-  const userProfileStores = UserProfileStores()
-  // 如果auth未啟動，就先啟動
+  const userStore = UserStore()
   document.title = `${to.meta.title} ▷One Bird WEB◁`
-  if (!userProfileStores.authOn) {
-    // 在onAuthStateChanged的函式內已經不是router了
-    // 所以必須使用router的功能去push新頁面，不能直接return路由名稱
+  if (!userStore.authOn) {
     onAuthStateChanged(auth, (res) => {
       if (res) {
-        userProfileStores.user = res
         console.log(`sign in ${res.email}`)
-        userProfileStores.authOn = true
-        if (res.email === null && res.displayName === null) {
-          userProfileStores.user.displayName = '訪客'
-        } else if (res.email !== null && res.displayName === null) {
-          userProfileStores.user.displayName = res.email
-        }
-        // 引入資料庫，在登入的時候就把帳戶data存到store
+        userStore.authOn = true
         import('@/api/firebase/database/user.js').then((module) => {
           const userFirebase = new module.default()
-          userFirebase.get(`${res.uid}/profile`).then((user) => {
-            // user是資料庫，res是auth的資料
-            // 有相同的資料時要以auth的資料為主，所以res放後面
-            userProfileStores.user = { ...user, ...res }
-
-            // 判斷不是訪客卻沒有一般權限的帳戶(剛從訪客升級為一般的用戶)
-            // 這邊不會幫存回資料庫，要等用戶有進行儲存相關操作才會存
-            if (!userProfileStores.user.isAnonymous) {
-              if (userProfileStores.user.role.indexOf('client') <= -1) {
-                userProfileStores.user.role.push('client')
+          userFirebase
+            .get(res.uid)
+            .then((user: User) => {
+              userStore.user = user
+              if (!comparisonRoles(to.meta.roles as string, userStore.user.roles)) {
+                console.log('no role [1]')
+                // router.push({ name: '403' })
               }
-            }
-            // 判斷有無認證信箱
-            // 同訪客，不會自動存回
-            if (userProfileStores.user.emailVerified) {
-              if (userProfileStores.user.role.indexOf('verify') <= -1) {
-                userProfileStores.user.role.push('verify')
-              }
-            }
-            // 有登入，判斷權限
-            if (!comparisonRole(to.meta.role as string, userProfileStores.user.role)) {
-              console.log('no role')
-              router.push({ name: '403' })
-            }
-          })
+            })
+            // 如果資料庫找不到，幫他創
+            .catch((error) => {
+              console.log(error)
+              userFirebase.setUser(res, []).then(() => {
+                // 創完重get一次
+                userFirebase.get(res.uid).then((user: User) => {
+                  userStore.user = user
+                  if (!comparisonRoles(to.meta.roles as string, userStore.user.roles)) {
+                    console.log('no role [3]')
+                    // router.push({ name: '403' })
+                  }
+                })
+              })
+            })
         })
       } else {
-        userProfileStores.user = null
-        userProfileStores.authOn = true
-        // 沒登入，送回登入
-        // 判斷權限的前置是要有登入，所以這裡擋沒登入的就好
-        if (to.meta.auth && userProfileStores.user === null) {
-          console.log('please log in')
+        userStore.user = { ...UserObject }
+        userStore.authOn = true
+        if (to.meta.auth && userStore.user.uid === '') {
+          console.log('please log in [1]')
           // router.push('/auth/login')
         }
       }
     })
   } else {
-    // 頁面要求登入 & 用戶沒登入 & 用戶權限與頁面權限不相等
-    if (to.meta.auth && userProfileStores.user === null) {
-      console.log('please log in')
+    if (to.meta.auth && userStore.user === null) {
+      console.log('please log in [2]')
       // return { name: 'Login' }
-    } else if (userProfileStores.user !== null) {
-      if (!comparisonRole(to.meta.role as string, userProfileStores.user.role)) {
-        console.log('no role')
+    } else if (userStore.user !== null) {
+      if (!comparisonRoles(to.meta.roles as string, userStore.user.roles)) {
+        console.log('no role [2]')
         // return { name: '403' }
       }
     }
   }
 })
 
-// userRole只要有一個權限符合routerRole，就可以進入
-// 會先判斷該頁面是否有設權限，無則直接通過
-function comparisonRole(routerRole: string, userRole: string[]) {
-  if (routerRole == undefined) {
+function comparisonRoles(routerRoles: string, userRoles: string[]): boolean {
+  if (!routerRoles) {
     return true
   }
-  let result = userRole.some((e) => {
-    return routerRole.indexOf(e) > -1
-  })
-  return result
+  return userRoles.some((e) => routerRoles.includes(e))
 }
 
 export default router
