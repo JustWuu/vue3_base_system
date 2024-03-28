38e27abff9a187b915e88963356d046df908da52
wu <rty21785@gmail.com>
2024-03-29 00:57:49 +0800

    feat(interface): 新增共用物件

--- src/interface/Universal.ts
+++ src/interface/Universal.ts
@@ -2,3 +2,9 @@
 export interface StringObject {
   [key: string]: string
 }
+
+export const StateArray: object[] = [
+  { name: '啟用', value: 'enable' },
+  { name: '停用', value: 'disabled' }
+  // { name: '刪除', value: 'delete' }
+]
