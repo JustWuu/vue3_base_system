f327f9728beaab7a313ff5cfb1eebe7892910872
wu <rty21785@gmail.com>
2024-04-07 02:46:46 +0800

    feat(user): 建立帳號功能

--- src/views/system/user/components/UserPage.vue
+++ src/views/system/user/components/UserPage.vue
@@ -1,321 +1,166 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { Auth, CRUDFirebase } from '@/api/firebase'
-import { Storage, Encryp } from '@/utils'
-import InputTextFloat from '@/components/form/InputTextFloat.vue'
-import InputPasswordFloat from '@/components/form/InputPasswordFloat.vue'
+import { Auth, UserFirebase } from '@/api'
+import {
+  InputPasswordFloat,
+  InputTextFloat,
+  InputDropdownFloat,
+  RoleDropdownFloat
+} from '@/components/form'
+import type { Account } from '@/interface'
+import { StateArray } from '@/interface'
+import { useRoute, useRouter } from 'vue-router'
+import { success, error } from '@/utils'
 
-const storage = new Storage()
-const encryp = new Encryp()
+const route = useRoute()
+const router = useRouter()
+// props
+const props = defineProps({
+  mode: {
+    type: String,
+    default: ''
+  }
+})
+
 // firebase
 const auth = new Auth()
-const crudFirebase = new CRUDFirebase()
+const userFirebase = new UserFirebase()
 
 //data
-const signupAccount = ref({
-  email: '',
-  password: ''
-})
+const id = ref('')
 
-const signinAccount = ref({
+const account = ref<Account>({
+  displayName: '',
   email: '',
   password: '',
-  checked: false
-})
-
-const uidText = ref('')
-
-interface CRUDTest {
-  doc: string
-  obj: {
-    [key: string]: string | number
-  }
-  array: object[]
-}
-
-const curdTest = ref<CRUDTest>({
-  doc: '',
-  obj: {
-    a: '',
-    b: '',
-    c: '',
-    d: 0
-  },
-  array: []
+  state: 'enable',
+  role: { displayName: '無', id: '' },
+  photoURL: ''
 })
 
 //methods
-const signup = () => {
-  auth
-    .createUser(signupAccount.value.email, signupAccount.value.password)
-    .then((res) => {
-      if (res) {
-        console.log(res)
-        // emit('showSuccess', '完成註冊，登入成功')
-        // message.sendSystemMail(mail.value)
-        // router.push('/')
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-      // emit('showError', error)
-    })
-}
-
-const signin = () => {
-  auth
-    .signIn(signinAccount.value.email, signinAccount.value.password)
-    .then((res) => {
-      if (res) {
-        console.log(res)
-        if (signinAccount.value.checked) {
-          signinAccount.value.password = encryp.aesEncryp(signinAccount.value.password)
-          storage.setLocalStorage('account', signinAccount.value)
-        } else {
-          storage.removeLocalStorage('account')
-        }
-        // emit('showSuccess', '登入成功')
-        // router.push('/')
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-      // emit('showError', error)
-    })
-}
-
-const signOut = () => {
-  auth
-    .signOut()
-    .then((res) => {
-      if (res) {
-        console.log(res)
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
-
-const getUid = (uid: string) => {
-  crudFirebase.get(uid).then((res) => {
-    uidText.value = res.uid
-  })
-}
-
-const getArray = () => {
-  crudFirebase.array().then((res) => {
-    curdTest.value.array = res
-    console.log(curdTest.value.array)
-  })
-}
-
-const get = (doc: string) => {
-  crudFirebase.get(doc).then((res) => {
-    console.log('資料：', res)
-  })
-}
-
-// crud
-const set = (id: string, params: object) => {
-  if (id === '') {
-    crudFirebase
-      .add(params)
+const submit = async (params: Account) => {
+  if (props.mode === 'add') {
+    auth
+      .createUser(params)
       .then((res) => {
-        console.log(res)
+        if (res) {
+          success(res)
+          router.push('/system/user/list')
+        }
       })
-      .catch((error) => {
-        console.log(error)
+      .catch((e) => {
+        error(e)
       })
-  } else {
-    crudFirebase.set(id, params)
+  } else if (props.mode === 'edit') {
+    userFirebase
+      .update(id.value, params)
+      .then((res) => {
+        success(res)
+        router.push('/system/user/list')
+      })
+      .catch((e) => {
+        error(e)
+      })
   }
 }
 
-const update = (id: string, params: object) => {
-  crudFirebase.update(id, params)
-}
-
-const remove = (id: string) => {
-  crudFirebase.delete(id)
-}
-
-// 下方onSnapshot寫法，必須搭配一個callback
-const onSnapshot = (id: string, callback: Function) => {
-  crudFirebase.onSnapshot(id, callback)
-}
-function callback(snapshot: any) {
-  curdTest.value.obj = snapshot
-}
-
-const arrayOnSnapshot = (callback: Function, id: string = '') => {
-  crudFirebase.arrayOnSnapshot(callback, id)
-}
-function callback2(snapshot: any) {
-  console.log(snapshot)
-}
 // onMounted
 onMounted(() => {
-  if (storage.getLocalStorage('account')) {
-    signinAccount.value = storage.getLocalStorage('account')
-    signinAccount.value.password = encryp.aesDecrypt(signinAccount.value.password)
+  if (props.mode === 'edit') {
+    id.value = route.params.id as string
+    userFirebase.get(id.value).then((res) => {
+      console.log(res)
+      account.value = res
+    })
   }
 })
 </script>
 
 <template>
   <div class="grid">
-    <div class="col-12 md:col-6 px-2">
-      <div class="card p-fluid">
-        <VForm ref="signupForm" @submit="signup()">
-          <h5>註冊</h5>
-          <div class="field mt-5">
-            <input-text-float
-              label="信箱"
-              v-model="signupAccount.email"
-              name="email0"
-              rules="required|email"
-            />
-          </div>
-          <div class="field mt-5">
-            <input-password-float
-              label="密碼"
-              v-model="signupAccount.password"
-              name="password0"
-              rules="required"
-            />
-          </div>
-          <Button label="註冊" type="submit"></Button>
-        </VForm>
-      </div>
-      <!--  -->
-      <div class="card p-fluid">
-        <VForm ref="signinForm" @submit="signin()" :key="signinAccount">
-          <h5>登入</h5>
-          <div class="field mt-5">
-            <input-text-float
-              label="信箱"
-              v-model="signinAccount.email"
-              name="email1"
-              rules="required|email"
-            />
-          </div>
-          <div class="field mt-5">
-            <input-password-float
-              label="密碼"
-              v-model="signinAccount.password"
-              name="password1"
-              rules="required"
-              :feedback="false"
-            />
-          </div>
-          <div class="flex align-items-center justify-content-between mb-4 gap-5">
-            <div class="flex align-items-center">
-              <Checkbox
-                v-model="signinAccount.checked"
-                id="rememberme1"
-                binary
-                class="mr-2"
-              ></Checkbox>
-              <label for="rememberme1">記住帳密</label>
+    <div class="col-12 px-2">
+      <VForm ref="signupForm" @submit="submit(account)" :key="account">
+        <div class="card p-fluid">
+          <h5 v-if="mode === 'add'">新增帳號</h5>
+          <h5 v-else-if="mode === 'edit'">編輯帳號</h5>
+          <div class="grid p-fluid mt-3">
+            <div class="col-12 md:col-6">
+              <input-text-float
+                label="名稱"
+                v-model="account.displayName"
+                name="displayName"
+                rules="required"
+              />
             </div>
-            <router-link
-              class="font-medium no-underline ml-2 text-right cursor-pointer"
-              style="color: var(--primary-color)"
-              to="/auth/forget"
-              >忘記密碼?</router-link
-            >
+            <div class="col-12 md:col-6">
+              <input-text-float
+                label="照片"
+                v-model="account.photoURL"
+                name="photoURL"
+                rules="required"
+              />
+            </div>
+            <div class="col-12 md:col-6">
+              <input-text-float
+                label="信箱"
+                v-model="account.email"
+                name="email"
+                rules="required|email"
+              />
+            </div>
+            <div class="col-12 md:col-6">
+              <input-password-float
+                v-if="mode === 'add'"
+                label="密碼"
+                v-model="account.password"
+                name="password"
+                rules="required"
+              />
+            </div>
+            <div class="col-12 md:col-6">
+              <input-dropdown-float
+                label="狀態"
+                :options="StateArray"
+                v-model="account.state"
+                name="state"
+                rules="required"
+              />
+            </div>
+            <div class="col-12 md:col-6">
+              <role-dropdown-float
+                label="權限"
+                v-model="account.role"
+                name="role"
+                rules="required"
+              />
+            </div>
           </div>
-          <Button label="登入" type="submit"></Button>
-        </VForm>
-      </div>
-    </div>
-
-    <div class="col-12 md:col-6 px-2">
-      <div class="card p-fluid">
-        <VForm ref="formRef" @submit="signOut()">
-          <h5>當前用戶</h5>
-          <div>
-            <p class="mb-0">asd</p>
-            <p class="mb-0">qwdqwd</p>
-            <p class="mb-0">qwdqwd</p>
-          </div>
-          <Button label="登出" severity="danger" type="submit"></Button>
-        </VForm>
-      </div>
-
-      <div class="card p-fluid">
-        <VForm ref="signupForm" @submit="getUid(uidText)">
-          <h5>撈取指定用戶資料</h5>
-          <div class="field mt-5">
-            <input-text-float label="UID" v-model="uidText" name="uid" rules="required" />
-          </div>
-          <Button label="取得" type="submit"></Button>
-        </VForm>
-      </div>
-      <div class="card p-fluid">
-        <h5>CRUD</h5>
-        <div class="field mt-5">
-          <input-text-float label="DOC" v-model="curdTest.doc" name="doc" rules="required" />
+          <Button label="送出" type="submit"></Button>
         </div>
-        <div class="field mt-5">
-          <input-text-float label="A" v-model="curdTest.obj.a" name="a" rules="required" />
-        </div>
-        <div class="field mt-5">
-          <input-text-float label="B" v-model="curdTest.obj.b" name="b" rules="required" />
-        </div>
-        <div class="field mt-5">
-          <input-text-float label="C" v-model="curdTest.obj.c" name="c" rules="required" />
-        </div>
-        <div class="field mt-5">
-          <input-text-float
-            label="D"
-            v-model="curdTest.obj.d"
-            name="d"
-            rules="required"
-            type="number"
-          />
-        </div>
-        <Button
-          label="新增"
-          type="submit"
-          class="mb-1"
-          severity="success"
-          @click="set(curdTest.doc, curdTest.obj)"
-        ></Button>
-        <Button label="取得陣列" type="submit" class="mb-1" @click="getArray()"></Button
-        ><Button label="取得單一" type="submit" class="mb-1" @click="get(curdTest.doc)"></Button>
-        <Button
-          label="修改"
-          type="submit"
-          class="mb-1"
-          severity="warning"
-          @click="update(curdTest.doc, curdTest.obj)"
-        ></Button>
-        <Button
-          label="刪除"
-          type="submit"
-          class="mb-1"
-          severity="danger"
-          @click="remove(curdTest.doc)"
-        ></Button>
-        <Button
-          label="監聽"
-          type="submit"
-          class="mb-1"
-          severity="danger"
-          @click="onSnapshot(curdTest.doc, callback)"
-        ></Button>
-        <Button
-          label="監聽陣列"
-          type="submit"
-          class="mb-1"
-          severity="danger"
-          @click="arrayOnSnapshot(callback2)"
-        ></Button>
-      </div>
+      </VForm>
     </div>
   </div>
+
+  <!-- <div class="grid">
+    <div v-role="['user:c']" class="col-12 md:col-6 px-2">
+      <div v-if="mode === 'add'" class="card p-fluid">
+        <VForm ref="signupForm" @submit="submit(account)">
+          <h5>登入信箱及密碼</h5>
+          <div class="field mt-5">
+            
+          </div>
+          <div class="field mt-5">
+            
+          </div>
+          <div class="field mt-5">
+            
+          </div>
+          <Button label="建立" type="submit"></Button>
+        </VForm>
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <style lang="scss" scoped></style>
