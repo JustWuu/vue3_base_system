9e0b0813c1f265a0a212c6238a7c4d79feb1278d
wu <rty21785@gmail.com>
2024-04-07 02:38:47 +0800

    feat(components): 權限dropdown做export

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,5 +2,12 @@ import InputDropdownFloat from './InputDropdownFloat.vue'
 import InputMultiSelectFloat from './InputMultiSelectFloat.vue'
 import InputPasswordFloat from './InputPasswordFloat.vue'
 import InputTextFloat from './InputTextFloat.vue'
+import RoleDropdownFloat from './RoleDropdownFloat.vue'
 
-export { InputDropdownFloat, InputMultiSelectFloat, InputPasswordFloat, InputTextFloat }
+export {
+  InputDropdownFloat,
+  InputMultiSelectFloat,
+  InputPasswordFloat,
+  InputTextFloat,
+  RoleDropdownFloat
+}
