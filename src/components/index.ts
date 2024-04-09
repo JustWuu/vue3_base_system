34c096f8a89a7746b0a4ed5f85f9b26837c88095
wu <rty21785@gmail.com>
2024-04-10 00:55:58 +0800

    feat(components): 增加文字顯示元件

---
+++ src/components/index.ts
@@ -0,0 +1,22 @@
+// form
+import InputDropdownFloat from './form/InputDropdownFloat.vue'
+import InputMultiSelectFloat from './form/InputMultiSelectFloat.vue'
+import InputPasswordFloat from './form/InputPasswordFloat.vue'
+import InputTextFloat from './form/InputTextFloat.vue'
+import RoleDropdownFloat from './form/RoleDropdownFloat.vue'
+import StateDropdownFloat from './form/StateDropdownFloat.vue'
+// table
+import UniversalTable from './table/UniversalTable.vue'
+// universal
+import FloatText from './universal/FloatText.vue'
+
+export {
+  InputDropdownFloat,
+  InputMultiSelectFloat,
+  InputPasswordFloat,
+  InputTextFloat,
+  RoleDropdownFloat,
+  StateDropdownFloat,
+  UniversalTable,
+  FloatText
+}
---
+++ src/components/universal/FloatText.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+//define
+defineProps({
+  label: {
+    type: String,
+    default: '-'
+  },
+  text: {
+    type: [String, Number],
+    default: '-'
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <div class="relative">
+    <span class="absolute float-span">{{ label }}</span>
+    <div class="p-inputtext" :class="{ 'opacity-90': disabled }">
+      <span class="text-base">{{ text }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.p-inputtext {
+  height: 40px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  // pointer-events: none;
+}
+.p-inputtext::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+  background-color: #c2c2c2;
+}
+.p-inputtext::-webkit-scrollbar-thumb {
+  width: 1px;
+  background-color: #6b7280;
+}
+
+.float-span {
+  position: absolute;
+  pointer-events: none;
+  margin-top: -0.5rem;
+  line-height: 1;
+  top: -0.75rem;
+  left: 0.75rem;
+  font-size: 12px;
+  color: #6b7280;
+}
+</style>
