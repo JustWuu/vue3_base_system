196e7ca8b145b7c0732acc5422aa2ef90624c812
wu <rty21785@gmail.com>
2024-04-15 23:57:16 +0800

    feat(auth): 增加信箱驗證、變更密碼

--- src/api/firebase/utils/useAuth.ts
+++ src/api/firebase/utils/useAuth.ts
@@ -4,24 +4,24 @@ import {
   browserSessionPersistence,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut
-  // deleteUser,
-  // reauthenticateWithCredential,
-  // EmailAuthProvider,
-  // updateProfile,
-  // updatePassword,
-  // sendEmailVerification,
-  // sendPasswordResetEmail,
+  signOut,
+  deleteUser,
+  reauthenticateWithCredential,
+  EmailAuthProvider,
+  updateProfile,
+  updatePassword,
+  sendEmailVerification,
+  sendPasswordResetEmail
   // signInAnonymously,
   // linkWithCredential
 } from 'firebase/auth'
 import { UserStore } from '@/stores'
 import { storeToRefs } from 'pinia'
 import { UserFirebase } from '@/api'
-import type { StringObject, User } from '@/interface'
-import { DebounceStore } from '@/stores'
+import { type StringObject, type User, UserObject } from '@/interface'
+import { LoadingStore } from '@/stores'
 
-const debounceStore = DebounceStore()
+const loadingStore = LoadingStore()
 
 // 註冊使用
 import { initializeApp } from 'firebase/app'
@@ -39,7 +39,7 @@ const authMessage: StringObject = {
   'auth/invalid-email': '無效的電子郵件',
   'auth/wrong-password': '密碼錯誤',
   'auth/weak-password': '密碼複雜度過低',
-  'auth/too-many-requests': '與先前發送間隔過短',
+  'auth/too-many-requests': '請求過於頻繁',
   'auth/missing-email': '請輸入信箱',
   'auth/user-not-7': '找不到該帳號',
   'auth/user-disabled': '該帳號已停權',
@@ -51,7 +51,7 @@ class Auth {
   // 管理帳號使用(不需依當前登入的帳號來做動)
   // 創建帳戶(不會直接登入)
   async createUser(account: User) {
-    debounceStore.debounce = true
+    loadingStore.debounce = true
     // 建立臨時實例
     const config = {
       apiKey: import.meta.env.VITE_API_KEY,
@@ -92,155 +92,152 @@ class Auth {
   // }
 
   // 當前帳號使用(需依當前登入的帳號來做動)
-  //登入
+  // 登入
   async signIn(email: string, password: string) {
-    debounceStore.debounce = true
+    loadingStore.debounce = true
     return await setPersistence(auth, browserSessionPersistence)
       .then(() => {
         return signInWithEmailAndPassword(auth, email, password)
           .then((userCredential) => {
             user = userCredential.user
-            debounceStore.debounce = false
+            loadingStore.debounce = false
             return `Sign-in ${userCredential.user.email}`
           })
           .catch((error) => {
             const errorCode = error.code
-            debounceStore.debounce = false
+            loadingStore.debounce = false
             throw authMessage[`${errorCode}`]
           })
       })
       .catch((error) => {
         const errorCode = error.code
-        debounceStore.debounce = false
+        loadingStore.debounce = false
         throw authMessage[`${errorCode}`]
       })
-    // return await signInWithEmailAndPassword(auth, email, password)
-    //   .then((userCredential) => {
-    //     user = userCredential.user
-    //     return `Sign-in ${userCredential.user.email}`
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code
-    //     throw authMessage[`${errorCode}`]
-    //   })
   }
-  //登入
+  // 登入
   async keepSignIn(email: string, password: string) {
-    debounceStore.debounce = true
+    loadingStore.debounce = true
     return await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         user = userCredential.user
-        debounceStore.debounce = false
+        loadingStore.debounce = false
         return `Sign-in ${userCredential.user.email}`
       })
       .catch((error) => {
         const errorCode = error.code
-        debounceStore.debounce = false
+        loadingStore.debounce = false
         throw authMessage[`${errorCode}`]
       })
   }
-  //登出
+  // 登出
   async signOut() {
-    debounceStore.debounce = true
+    loadingStore.debounce = true
     return await signOut(auth)
       .then(() => {
-        debounceStore.debounce = false
+        loadingStore.debounce = false
         return 'Sign-out successful'
       })
       .catch((error) => {
         const errorMessage = error.message
-        debounceStore.debounce = false
+        loadingStore.debounce = false
         return errorMessage
       })
   }
-  // //重新驗證帳戶(進行敏感操作時)
-  // async reauthenticate(password) {
-  //   user = auth.currentUser
-  //   const credential = EmailAuthProvider.credential(user.email, password)
-  //   return await reauthenticateWithCredential(user, credential)
-  //     .then((userCredential) => {
-  //       console.log(`${user.email} reauthenticate OK`)
-  //       return userCredential.user
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code
-  //       throw authMessage[`${errorCode}`]
-  //     })
-  // }
-  // //變更密碼
-  // async updatePassword(newPassword) {
-  //   user = auth.currentUser
-  //   return await updatePassword(user, newPassword)
-  //     .then(() => {
-  //       console.log(`${user.email} updatePassword OK`)
-  //       return true
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code
-  //       throw authMessage[`${errorCode}`]
-  //     })
-  // }
-  // //修改帳戶資料
-  // async update(profile) {
-  //   user = auth.currentUser
-  //   return await updateProfile(user, profile)
-  //     .then(() => {
-  //       userStore.user = null
-  //       userStore.user = { ...profile, ...user }
-  //       if (userStore.user.displayName === null) {
-  //         userStore.user.displayName = userStore.user.email
-  //       }
-  //       console.log(`${user.email} update OK`)
-  //       userFirebase.updateUserProfile(profile)
-  //       return true
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code
-  //       throw authMessage[`${errorCode}`]
-  //     })
-  // }
-  // //刪除帳號，刪除帳號並不會連同資料一起刪除
-  // //而是會在資料上備註已刪除
-  // async deleteUser() {
-  //   const today = new Date()
-  //   user = auth.currentUser
-  //   const uid = user.uid
-  //   return await deleteUser(user)
-  //     .then(() => {
-  //       userFirebase.update(`${uid}/profile`, { state: 'delete', updateAt: today })
-  //       return true
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code
-  //       throw authMessage[`${errorCode}`]
-  //     })
-  // }
-  // //信箱驗證
-  // async sendEmail() {
-  //   user = auth.currentUser
-  //   return await sendEmailVerification(user)
-  //     .then(() => {
-  //       console.log(`${user.email} send email OK`)
-  //       return true
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code
-  //       throw authMessage[`${errorCode}`]
-  //     })
-  // }
-  // //忘記密碼
-  // async sendPassword(email) {
-  //   return await sendPasswordResetEmail(auth, email)
-  //     .then(() => {
-  //       console.log(`${email} send password OK`)
-  //       return true
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code
-  //       throw authMessage[`${errorCode}`]
-  //     })
-  // }
-  // //訪客登入
+  // 重新驗證帳戶(進行敏感操作時)
+  async reauthenticate(password: string) {
+    user = auth.currentUser
+    const credential = EmailAuthProvider.credential(user!.email!, password)
+    return await reauthenticateWithCredential(user!, credential)
+      .then((userCredential) => {
+        return userCredential.user
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        throw authMessage[`${errorCode}`]
+      })
+  }
+  // 變更密碼
+  async updatePassword(password: string, newPassword: string) {
+    user = auth.currentUser
+    loadingStore.debounce = true
+    return await this.reauthenticate(password)
+      .then(() => {
+        return updatePassword(user!, newPassword)
+          .then(() => {
+            console.log(`${user!.email} update-password`)
+            return true
+          })
+          .catch((error) => {
+            const errorCode = error.code
+            throw authMessage[`${errorCode}`]
+          })
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        loadingStore.debounce = false
+        throw authMessage[`${errorCode}`]
+      })
+  }
+  // 修改帳戶資料
+  async update(profile: User) {
+    console.log(profile)
+    user = auth.currentUser
+    return await updateProfile(user!, profile)
+      .then(() => {
+        userStore.user = UserObject
+        userStore.user = { ...profile }
+        console.log(`${user!.email} update-profile`)
+        userFirebase.update(user!.uid, profile)
+        return true
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        throw authMessage[`${errorCode}`]
+      })
+  }
+  // 刪除帳號，刪除帳號並不會連同資料一起刪除
+  // 而是會在資料上備註已刪除
+  async deleteUser() {
+    const today = new Date()
+    user = auth.currentUser
+    const uid = user!.uid
+    return await deleteUser(user!)
+      .then(() => {
+        userFirebase.update(uid, { state: 'delete', updateAt: today })
+        return true
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        throw authMessage[`${errorCode}`]
+      })
+  }
+  // 信箱驗證
+  async sendEmail() {
+    user = auth.currentUser
+    return await sendEmailVerification(user!)
+      .then(() => {
+        console.log(`${user!.email} send-email-verification OK`)
+        return true
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        throw authMessage[`${errorCode}`]
+      })
+  }
+  // 忘記密碼
+  async sendPassword(email: string) {
+    return await sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log(`${email} send-password-reset-email OK`)
+        return true
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        throw authMessage[`${errorCode}`]
+      })
+  }
+  // // 訪客登入
   // async signInAnonymously() {
   //   return await signInAnonymously(auth)
   //     .then((userCredential) => {
@@ -252,7 +249,7 @@ class Auth {
   //       throw authMessage[`${errorCode}`]
   //     })
   // }
-  // //訪客註冊
+  // // 訪客註冊
   // async anonymouslyRegister(email, password) {
   //   const credential = EmailAuthProvider.credential(email, password)
   //   user = auth.currentUser
@@ -269,9 +266,9 @@ class Auth {
   //       throw authMessage[`${errorCode}`]
   //     })
   // }
-  //取得當前用戶
-  //storeToRefs只會淺監聽，所以改裡面得值並不會重給值
-  //需設成null再從給值才能監聽到變化，ex：修改帳戶資料
+  // 取得當前用戶
+  // storeToRefs只會淺監聽，所以改裡面得值並不會重給值
+  // 需設成null再從給值才能監聽到變化，ex：修改帳戶資料
   // 使用storeToRefs可以確保資料為響應式，資料皆為最新的值
   getUser() {
     return storeToRefs(userStore)
