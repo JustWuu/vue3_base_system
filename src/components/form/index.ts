632c07bb5f610c5680cac06828dc9200f0ad146a
wu <rty21785@gmail.com>
2024-04-15 21:16:37 +0800

    fix(components): 移除不存在的import

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,5 +1,4 @@
 import InputDropdownFloat from './InputDropdownFloat.vue'
-import InputMultiSelectFloat from './InputMultiSelectFloat.vue'
 import InputPasswordFloat from './InputPasswordFloat.vue'
 import InputTextFloat from './InputTextFloat.vue'
 import RoleDropdownFloat from './RoleDropdownFloat.vue'
@@ -7,7 +6,6 @@ import StateDropdownFloat from './StateDropdownFloat.vue'
 
 export {
   InputDropdownFloat,
-  InputMultiSelectFloat,
   InputPasswordFloat,
   InputTextFloat,
   RoleDropdownFloat,
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 // form
 import InputDropdownFloat from './form/InputDropdownFloat.vue'
-import InputMultiSelectFloat from './form/InputMultiSelectFloat.vue'
 import InputPasswordFloat from './form/InputPasswordFloat.vue'
 import InputTextFloat from './form/InputTextFloat.vue'
 import RoleDropdownFloat from './form/RoleDropdownFloat.vue'
@@ -14,7 +13,6 @@ import FloatText from './universal/FloatText.vue'
 
 export {
   InputDropdownFloat,
-  InputMultiSelectFloat,
   InputPasswordFloat,
   InputTextFloat,
   RoleDropdownFloat,
